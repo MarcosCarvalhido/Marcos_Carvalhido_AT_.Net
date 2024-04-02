403f00b345ea56c34f4d5e69c77d046b99e8415e
Marcos Carvalhido Rodrigues <marcoscrodrigues8@gmail.com>
2024-04-02 17:54:07 -0300

    Add project files.

---
+++ InfnetMVC/Data/Aluno.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace InfnetMVC.Data
+{
+    public class Aluno
+    {
+        public int Id { get; set; }
+        [Display(Name = "Nome")]
+        [StringLength(50, MinimumLength = 4, ErrorMessage = "Nome deve ter entre 4 e 50 caracteries.")]
+        public string Name { get; set; }
+        public string Endereco { get; set; }
+        public string Telefone { get; set; }
+        [DataType(DataType.EmailAddress)]
+        public string email { get; set; }
+        [Display(Name = "Data De Nascimento")]
+        [DataType(DataType.Date)]
+        public string Nascimento { get; set; }
+    }
+}
---
+++ InfnetMVC/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using InfnetMVC.Data;
+
+namespace InfnetMVC.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<InfnetMVC.Data.Aluno> Aluno { get; set; } = default!;
+    }
+}
